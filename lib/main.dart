9bc8ed7a5f2a91765264bd1431fa32984648342b
Juliusz Piskor <juliuszpiskor@gmail.com>
2024-07-13 00:00:04 +0200

    firebase fix

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,14 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:motorappka/core/app.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:timezone/timezone.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
