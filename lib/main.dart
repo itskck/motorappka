b553e81ca3bd8d9485da7a6ef0a77bcb42cbda13
Juliusz Piskor <juliuszpiskor@gmail.com>
2024-11-13 09:25:12 +0100

    feature: hid api keys to make project public

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:motorappka/core/app.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,12 +11,15 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
+
+  await dotenv.load();
 
   final ByteData tzf = await rootBundle.load('assets/timezone/latest.tzf');
   initializeDatabase(tzf.buffer.asUint8List());
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Constants {
   static const String appName = 'Motorappka';
   static const String appAuthor = 'Juliusz Piskor';
@@ -5,7 +7,7 @@ class Constants {
   static const String motorSportsApiId = '147435';
   static const String ekstraklasaSportsApiId = '4422';
   static const String sportsApiBaseUrl = 'https://www.thesportsdb.com';
-  static const String sportsApiKey = '980201';
+  static final String sportsApiKey = dotenv.env['API_KEY'] ?? '';
   static const String currentSeason = '2024-2025';
 
   // Contentful
